4b2e1fa6bc7986e092e423ea1cf75461938c259f
Gwenn-LR <gwenn.le-roch@isen-ouest.yncrea.fr>
2022-03-30 01:10:04 +0200

    Modèle CNN OK

--- cnn.py
+++ cnn.py
@@ -1,4 +1,56 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers, models
+from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
+import data
+import datetime
+
+
+def run_app():
+    X_train, X_test, y_train, y_test = data.chargementImagesTrainTest("data")
+
+    y_train = to_categorical(y_train, num_classes=10)
+    y_test = to_categorical(y_test, num_classes=10)
+
+    input_shape = X_train[0].shape
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+    print(model.summary())
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+
+    print(model.summary())
+
+    model.compile(optimizer='adam',
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+    history = model.fit(X_train, y_train, epochs=10, 
+                    validation_data=(X_test, y_test))
+
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+    test_loss, test_acc = model.evaluate(X_test,  y_test, verbose=2)
+
+    print(test_acc)
+    
+    now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    model.save(f"models/{now}-{test_acc:.2f}")
+
+
+if __name__ == "__main__":
+    run_app()
